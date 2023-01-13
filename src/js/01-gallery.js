b25a5519efaa0b49ce87477a8c28727de9b93048
Santiago <santiago@192.168.0.5>
2023-01-13 12:08:01 -0500

    tarea1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,34 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+const gallery = document.querySelector(".gallery");
+
+for (let item of galleryItems) {
+
+    const link = document.createElement("a");
+    link.classList.add("gallery__item");
+    link.href = item.original;
+    gallery.append(link);
+
+    const image = document.createElement("img");
+    image.classList.add("gallery__image");
+    image.src = item.preview;
+    image.alt = item.description;
+    link.append(image);
+    
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+    });
+};
+
+
+let lightbox = new SimpleLightbox('.gallery a', { gallery });
+lightbox.options.captionsData = "alt";
+lightbox.options.captionDelay = 250;
+
+
+console.log(lightbox);
+console.log(gallery);
